8c956b80a242300fbcd5d3014d566ad0f218e968
sajidannn <ahmadnursajidan@gmail.com>
2023-10-08 13:06:33 +0700

    initial commit

---
+++ src/script.js
@@ -0,0 +1,47 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+const tab1 = document.getElementById("tab1");
+const tab2 = document.getElementById("tab2");
+const tab3 = document.getElementById("tab3");
+
+function handleTabClick(tabElement) {
+  showTab(tabElement);
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+  hamburger.classList.toggle("toggle");
+}
+
+function showTab(tabToShow) {
+  tab1.style.display = "none";
+  tab2.style.display = "none";
+  tab3.style.display = "none";
+
+  tabToShow.style.display = "block";
+}
+
+// Tambahkan event listener untuk navbar atau elemen yang memicu pergantian tab
+document.getElementById("navTab1").addEventListener("click", function () {
+  showTab(tab1);
+  handleTabClick(tab1);
+});
+
+document.getElementById("navTab2").addEventListener("click", function () {
+  showTab(tab2);
+  handleTabClick(tab2);
+});
+
+document.getElementById("navTab3").addEventListener("click", function () {
+  showTab(tab3);
+  handleTabClick(tab3);
+});
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+  hamburger.classList.toggle("toggle");
+});
